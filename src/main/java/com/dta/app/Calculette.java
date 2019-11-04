822c58f6c41c04844d396d9b487cc5d936a45c9b
Fabrice Reigner <fabricereigner49@msn.com>
2019-11-04 11:56:05 +0100

    fix code

--- src/main/java/com/dta/app/Calculette.java
+++ src/main/java/com/dta/app/Calculette.java
@@ -2,16 +2,16 @@ package com.dta.app;
 
 public class Calculette {
 
-        int a = 12;
-        int b = 3;
+        final int a = 12;
+        final int b = 3;
         int sumAB = sum(a, b);
         int multiAB = multiplication(a, b);
 
-    public static int sum(int a, int b) {
+    public final int sum(int a, int b) {
         int result = (a+b);
         return result;
     }
-    public static int multiplication(int a,int b) {
+    public final int multiplication(int a,int b) {
         int result = (a*b);
         return result;
     }
